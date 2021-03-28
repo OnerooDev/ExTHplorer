ea7012c3e4e06f0447b024194a6d0bb6da682c48
SeerDay <70478004+SeerDay@users.noreply.github.com>
2021-03-29 03:25:31 +0700

    Exthplorer release v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # ExTHplorer
- 
+
+####More usefull api requests for explore smart-contracts on ETH/BSC/MATIC and same EVMs.
+
+## Install
+  Yarn
+
+  node exth.js
+
+----
+
+## API Scheme
+####Standart URL address - http://localhost:3001/api3/{PATH}
+----
+####All available **GET** requests:
+----
+Use this **{PATH}** to prepare your request:
+
+User data description:
+**$contr_address** - smart-contract address
+**$user_address** - user address
+
+#####🦄1. {PATH} = UNI/{METH}
+Methods for explore 🦄Uniswap🦄 smart-contracts:
+
+Uniswap V2:
+**- {METH} = V2/{FUNC}**
+
+Get pool reserves from LP contract
+**{FUNC} = reserves/$contr_address**
+
+Get pool tokens addresses from LP contract
+**{FUNC} = tokens/$contr_address**
+
+Get pool prices from LP contract
+**{FUNC} = prices/$contr_address**
+
+
+Get ALL ⬆️ **{FUNC}** from LP contract
+**{FUNC} = info/$contr_address**
+
+
+
+Uniswap V3:
+**- {METH} = V3/{FUNC}**
+*Maintenance 🔧*
+#####⛓2. {PATH} = TOKEN/{METH}
+Methods for ⛓Token⛓ smart-contract
+
+Get token($contr_address) name and symbol
+**- {METH} = name/$contr_address**
+
+Get token($contr_address) total Supply
+**- {METH} = supply/$contr_address**
+
+Get token($contr_address) decimals
+**- {METH} = decimals/$contr_address**
+
+Get token($contr_address) balance on wallet($user_address)
+**- {METH} = balanceof/$contr_address/$user_address**
+
+*Maintenance 🔧*
+
+
+#####🌈3. {PATH} = CURVE/{METH}
+Methods for explore 🌈Curve🌈 smart-contracts
+
+*Maintenance 🔧*
---
+++ exth.js
@@ -0,0 +1,254 @@
+// Application for fast explore ETH/BSC/MATIC and same EVMs smart-contracts:
+// https://github.com/SeerDay
+
+// Before use you must to configure .env
+
+//Use lib
+require('dotenv').config();
+const express = require("express");
+const ethers = require('ethers');
+var networks = require('@ethersproject/networks');
+var app = express();
+var abi = require('./lib/contract-abi.js');
+var decoder = require('./lib/decode.js');
+
+//
+
+//Connect to Blockchain
+function BlocksConnect() {
+	//Use URL from .env for make connect to RPC-node
+	const provider = new ethers.providers.getDefaultProvider(process.env.URL);
+	//Use PRIVATE_KEY from .env for make account connected to $provider
+	const signer = new ethers.Wallet(process.env.PRIVATE_KEY);
+	const account = signer.connect(provider);
+	//back
+	return account;
+}
+//Get reserves from UNIv2 smart-contract
+//
+async function getReservesUNI_V2(cont_address) {
+    const account = BlocksConnect();
+		//
+		const Contract = abi.UNI_V2(cont_address, account);
+		const reserves = await Contract.getReserves();
+		const res0token = decoder.to_string(reserves._reserve0);
+		const res1token = decoder.to_string(reserves._reserve1);
+		//
+		let obj_res = {
+			token0: res0token,
+			token1: res1token
+		};
+		let finish = {
+			reserves: obj_res
+		};
+
+		//back
+		return finish;
+}
+//Get tokens address from UNIv2 smart-contract
+//
+async function getTokensUNI_V2(cont_address) {
+    const account = BlocksConnect();
+		//
+		const Contract = abi.UNI_V2(cont_address, account);
+		const token0 = await Contract.token0();
+		const token1 = await Contract.token1();
+		const res0token = decoder.to_string(token0);
+		const res1token = decoder.to_string(token1);
+		//
+		let obj_res = {
+			token0: res0token,
+			token1: res1token
+		};
+		let finish = {
+			addresses: obj_res
+		};
+
+		//back
+		return finish;
+}
+//Get tokens address from UNIv2 smart-contract
+//
+async function getPriceUNI_V2(cont_address) {
+    const account = BlocksConnect();
+		//
+		const Contract = abi.UNI_V2(cont_address, account);
+		const price0 = await Contract.price0CumulativeLast();
+		const price1 = await Contract.price1CumulativeLast();
+		const res0price = decoder.to_string(price0);
+		const res1price = decoder.to_string(price1);
+		//
+		let obj_res = {
+			token0: res0price,
+			token1: res1price
+		};
+		let finish = {
+			prices: obj_res
+		};
+		//back
+		return finish;
+}
+//Get tokens address from UNIv2 smart-contract
+//
+async function getInfoUNI_V2(cont_address) {
+		//
+		const reserves = await getReservesUNI_V2(cont_address);
+		const price = await getPriceUNI_V2(cont_address);
+		const token = await getTokensUNI_V2(cont_address);
+		//
+		let obj_res = {
+			res: reserves,
+			price: price,
+			token: token
+		};
+		let finish = {
+			info: obj_res
+		};
+		//back
+		return finish;
+}
+//Get tokens address from UNIv2 smart-contract
+//
+async function getTokenName(cont_address) {
+    const account = BlocksConnect();
+		//
+		const Contract = abi.token(cont_address, account);
+		const name = await Contract.name();
+		const name_dec = decoder.to_string(name);
+		//
+		let obj_res = {
+			name: name_dec
+		};
+		//back
+		return obj_res;
+}
+//Get tokens address from UNIv2 smart-contract
+//
+async function getTokenSupply(cont_address) {
+    const account = BlocksConnect();
+		//
+		const Contract = abi.token(cont_address, account);
+		const supply = await Contract.totalSupply();
+		const supply_dec = decoder.to_string(supply);
+		//
+		let obj_res = {
+			supply: supply_dec
+		};
+		//back
+		return obj_res;
+}
+//Get tokens address from UNIv2 smart-contract
+//
+async function getTokenDecimal(cont_address) {
+		const account = BlocksConnect();
+		//
+		const Contract = abi.token(cont_address, account);
+		const decimal = await Contract.decimals();
+		const decimal_dec = decoder.to_string(decimal);
+		//
+		let obj_res = {
+			decimals: decimal_dec
+		};
+		//back
+		return obj_res;
+}
+//Get tokens address from UNIv2 smart-contract
+//
+async function getTokenBalance(cont_address, user_address) {
+    const account = BlocksConnect();
+		//
+		const Contract = abi.token(cont_address, account);
+		const balance = await Contract.balanceOf(user_address);
+		const balance_dec = parseFloat(decoder.to_string(balance));
+		//
+		let obj_res = {
+			token: balance_dec
+		};
+		//back
+		return obj_res;
+}
+
+//ROUTES
+//
+//ApiRoutes
+//UNI
+app.get('/api3/UNI/V2/reserves/:cont_address',async function (req, res, next) {
+    var func = await getReservesUNI_V2(req.params.cont_address)
+
+    res.json(func)
+
+});
+
+app.get('/api3/UNI/V2/tokens/:cont_address',async function (req, res, next) {
+    var func = await getTokenUNI_V2(req.params.cont_address)
+
+    res.json(func)
+
+});
+
+app.get('/api3/UNI/V2/prices/:cont_address',async function (req, res, next) {
+    let func = await getPriceUNI_V2(req.params.cont_address)
+
+    res.json(func)
+
+});
+
+app.get('/api3/UNI/V2/info/:cont_address',async function (req, res, next) {
+    let func = await getInfoUNI_V2(req.params.cont_address)
+
+    res.json(func)
+
+});
+//
+//Token
+app.get('/api3/token/name/:cont_address',async function (req, res, next) {
+    let func = await getTokenName(req.params.cont_address)
+
+    res.json(func)
+
+});
+
+app.get('/api3/token/supply/:cont_address',async function (req, res, next) {
+    let func = await getTokenSupply(req.params.cont_address)
+
+    res.json(func)
+
+});
+
+app.get('/api3/token/decimals/:cont_address',async function (req, res, next) {
+    let func = await getTokenDecimal(req.params.cont_address)
+
+    res.json(func)
+
+});
+
+app.get('/api3/token/balanceof/:cont_address/:address',async function (req, res, next) {
+    let func = await getTokenBalance(req.params.cont_address, req.params.address)
+
+		let finish = {
+			balance: func
+		};
+
+    res.json(finish)
+
+});
+//
+
+//app.use(function(err, req, res, next) {
+//	res.status(500).send('Please update page')
+//});
+
+//
+//
+//
+
+//Start WebServer
+var server = app.listen(3001, function () {
+
+    var host = "localhost";
+    var port = server.address().port;
+
+    console.log("App listen at - http://" + host + ":" + port)
+
+});
+//
